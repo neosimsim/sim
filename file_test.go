64d0bf44da6c76e46f122a490cd26564ea53963c
Alexander Ben Nasrallah <an@sernet.de>
2018-05-25 09:08:14 +0200

    Add test for file.go

--- file_test.go
+++ file_test.go
@@ -53,3 +53,48 @@ func TestDeleteDot(t *testing.T) {
 		t.Error("Range not properly updated after deleting dot.")
 	}
 }
+
+func TestCopyDot(t *testing.T) {
+	file := File{Buffer: "Hello World!\n", Range: Range{5, 11}}
+	file.CopyDot(5)
+
+	if file.Buffer != "Hello World World!\n" {
+		t.Error("Wrong copy result. Expected ", "Hello Moon!", " got ", file.Buffer)
+	}
+	if file.Range.Start != 6 || file.Range.End != 10 {
+		t.Error("Range not properly updated after changing dot.")
+	}
+}
+
+func BenchmarkCopyDot(b *testing.B) {
+	file := File{Buffer: "Hello World!\n", Range: Range{6, 11}}
+	for i := 0; i < b.N; i++ {
+		file.Append("Moon")
+	}
+}
+
+func TestMoveDot(t *testing.T) {
+	file := File{Buffer: "Hello World!\n", Range: Range{6, 11}}
+	file.Change("Moon")
+
+	if file.Buffer != "Hello Moon!\n" {
+		t.Error("Wrong change result. Expected ", "Hello Moon!", " got ", file.Buffer)
+	}
+	if file.Range.Start != 6 || file.Range.End != 10 {
+		t.Error("Range not properly updated after changing dot.")
+	}
+}
+
+func BenchmarkMoveDot(b *testing.B) {
+	file := File{Buffer: "Hello World!\n", Range: Range{6, 11}}
+	for i := 0; i < b.N; i++ {
+		file.Append("Moon")
+	}
+}
+
+func TestDot(t *testing.T) {
+	file := File{Buffer: "Hello World!\n", Range: Range{3, 7}}
+	if file.Dot() != "lo W" {
+		t.Error("Unexcpeted content of Dot. Expected ", "lo W", " got ", file.Dot())
+	}
+}
