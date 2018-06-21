de36c281476564f572a70e2b1cd041d6cb9b3156
Alexander Ben Nasrallah <an@sernet.de>
2018-06-21 19:19:12 +0200

    Replace all file functions by set and get text

--- file_test.go
+++ file_test.go
@@ -4,110 +4,62 @@ import (
 	"testing"
 )
 
-func TestAppend(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.Append(" added content", a) }, "Hello Wo added content rld!")
+func TestSetText(t *testing.T) {
+	file := &File{buffer: "Hello World!", dot: Address{Start: 7, End: 11}}
+	addr, err := file.SetText(" new test content ", Address{Start: 3, End: 8})
+
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	if file.buffer != "Hel new test content rld!" {
+		t.Errorf("expected buffer '%s' got '%s'", "Hel new test content rld!", file.buffer)
+	}
+	if file.dot.Start != 7 || file.dot.End != 11 {
+		t.Errorf("expected dot to be unchanged #%d,#%d got #%d,#%d", 7, 11, file.dot.Start, file.dot.End)
+	}
+	if addr.Start != 3 || addr.End != 21 {
+		t.Errorf("expected returned address #%d,#%d got #%d,#%d", 3, 21, addr.Start, addr.End)
+	}
 }
 
-func TestChange(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.Change("changed content", a) }, "Hellchanged contentrld!")
+func TestSetTextInEmptyAddress(t *testing.T) {
+	file := &File{buffer: "Hello World!", dot: Address{Start: 7, End: 11}}
+	addr, err := file.SetText(" new test content ", Address{Start: 5, End: 5})
+
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	if file.buffer != "Hello new test content  World!" {
+		t.Errorf("expected buffer '%s' got '%s'", "Hel new test content rld!", file.buffer)
+	}
+	if file.dot.Start != 7 || file.dot.End != 11 {
+		t.Errorf("expected dot to be unchanged #%d,#%d got #%d,#%d", 7, 11, file.dot.Start, file.dot.End)
+	}
+	if addr.Start != 5 || addr.End != 23 {
+		t.Errorf("expected returned address #%d,#%d got #%d,#%d", 5, 23, addr.Start, addr.End)
+	}
 }
 
-func TestCopy(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.Copy(Address{4, 6}, a) }, "Hello lo WoWorld!")
-}
+func TestErrorOnInvalidAddress(t *testing.T) {
+	file := &File{buffer: "Hello World!", dot: Address{Start: 7, End: 11}}
 
-func TestDelete(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, file.Delete, "Helrld!")
-}
+	_, err := file.SetText(" new test content ", Address{Start: -1, End: 8})
+	if _, ok := err.(*InvalidAddressError); !ok {
+		t.Errorf("expected error of type *InvalidAddressError for Start < 0 got %T", err)
+	}
 
-func TestInsert(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.Insert(" inserted content ", a) }, "Hel inserted content lo World!")
-}
+	_, err = file.SetText(" new test content ", Address{Start: 30, End: 40})
+	if _, ok := err.(*InvalidAddressError); !ok {
+		t.Errorf("expected error of type *InvalidAddressError for Start > len got %T", err)
+	}
 
-func TestMove(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.Copy(Address{4, 9}, a) }, "Helrlo Wold!")
+	_, err = file.SetText(" new test content ", Address{Start: 1, End: 40})
+	if _, ok := err.(*InvalidAddressError); !ok {
+		t.Errorf("expected error of type *InvalidAddressError for End > len got %T", err)
+	}
 
-	assertFileChange(t)(file, func(a Address) error { return file.Copy(Address{4, 6}, a) }, "Hel lo World!")
-	t.Error("Address overlapping")
-}
-
-func TestPipe(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.Pipe("", a) }, "Hel loWorld!")
-	t.Error("Not implemented")
-}
-
-func TestPipeIn(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.PipeOut("", a) }, "Hel loWorld!")
-	t.Error("Not implemented")
-}
-
-func TestPipeOut(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.PipeIn("", a) }, "Hel loWorld!")
-	t.Error("Not implemented")
-}
-
-func TestPrint(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { file.Print(a); return nil }, "Hel loWorld!")
-	t.Error("Maybe Print should be called Dot")
-}
-
-func TestSetMark(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, file.SetMark, "Hel loWorld!")
-}
-
-func TestSubstitute(t *testing.T) {
-	file := NewTestFile()
-	assertFileChange(t)(file, func(a Address) error { return file.Substitute("..$", "xyz", a) }, "Hello xyzrld!")
-}
-
-func NewTestFile() *File {
-	return &File{buffer: "Hello World!", dot: Address{Start: 9, End: 11}}
-}
-
-func assertFileChange(t *testing.T) func(*File, func(Address) error, string) {
-	return func(file *File, f func(Address) error, expectedBuffer string) {
-		err := f(Address{Start: 3, End: 8})
-
-		if err != nil {
-			t.Errorf("Unexpected error %v", err)
-		}
-
-		if file.buffer != expectedBuffer {
-			t.Errorf("expected buffer '%s' got '%s'", expectedBuffer, file.buffer)
-		}
-		if file.dot.Start != 9 || file.dot.End != 11 {
-			t.Errorf("expected dot to be unchanged #%d,#%d got #%d,#%d", 9, 11, file.dot.Start, file.dot.End)
-		}
-
-		err = f(Address{Start: -1, End: 5})
-		if _, ok := err.(*InvalidAddressError); !ok {
-			t.Errorf("expected error of type *InvalidAddressError for Start < 0 got %T", err)
-		}
-
-		err = f(Address{Start: 30, End: 40})
-		if _, ok := err.(*InvalidAddressError); !ok {
-			t.Errorf("expected error of type *InvalidAddressError for Start > len got %T", err)
-		}
-
-		err = f(Address{Start: 1, End: 40})
-		if _, ok := err.(*InvalidAddressError); !ok {
-			t.Errorf("expected error of type *InvalidAddressError for End > len got %T", err)
-		}
-
-		err = f(Address{Start: 8, End: 4})
-		if _, ok := err.(*InvalidAddressError); !ok {
-			t.Errorf("expected error of type *InvalidAddressError for Start > End got %T", err)
-		}
+	_, err = file.SetText(" new test content ", Address{Start: 8, End: 4})
+	if _, ok := err.(*InvalidAddressError); !ok {
+		t.Errorf("expected error of type *InvalidAddressError for Start > End got %T", err)
 	}
 }
