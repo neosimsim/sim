92ac84dd8008a51946742e75981307321ab9b092
Alexander Ben Nasrallah <me@abn.sh>
2018-08-27 21:59:21 +0200

    fix package file

--- file/file.go
+++ file/file.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD 3-clause
 // style license that can be found in the LICENSE file.
 
-package sim
+package file
 
 import (
 	"strings"
--- file/file_test.go
+++ file/file_test.go
@@ -1,4 +1,4 @@
-package sim
+package file
 
 import (
 	"testing"
