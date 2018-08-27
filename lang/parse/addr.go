14b843fb349b6307ac359dec4ccc309874319cf6
Alexander Ben Nasrallah <me@abn.sh>
2018-08-27 21:28:09 +0200

    add package parse to lang/parse

--- lang/parse/addr.go
+++ lang/parse/addr.go
@@ -3,27 +3,27 @@ package parse
 import (
 	"regexp"
 
-	"github.com/neosimsim/sim"
+	"github.com/neosimsim/sim/file"
 )
 
 // AddressDesc is an interface to provide functions to scan a file for an
 // Address.
 type AddressDesc interface {
 	// Scan for the Address in File described by AddrresDesc after Address start.
-	Scan(f *sim.File, start sim.Address) (sim.Address, error)
+	Scan(f *file.File, start file.Address) (file.Address, error)
 }
 
 type RegexpOffset regexp.Regexp
-func (o RegexpOffset) Scan(f *sim.File, start sim.Address) (sim.Address, error) {
+func (o RegexpOffset) Scan(f *file.File, start file.Address) (file.Address, error) {
 	return 0, nil
 }
 
 type LineOffset uint
-func (o LineOffset) Scan(f *sim.File, start sim.Address) (sim.Address, error) {
+func (o LineOffset) Scan(f *file.File, start file.Address) (file.Address, error) {
 	return 0, nil
 }
 
 type CharOffset uint
-func (o CharOffset) Scan(f *sim.File, start sim.Address) (sim.Address, error) {
+func (o CharOffset) Scan(f *file.File, start file.Address) (file.Address, error) {
 	return 0, nil
 }
